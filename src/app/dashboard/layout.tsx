d07e146cb62f5c1ce79a28f48e631e80d51e2ba1
Alex Yaj <www.alex94lykam@gmail.com>
2025-12-22 11:55:53 +0000

    Restructure both the expanded and collapsed sidebar states to follow pro

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -91,13 +91,13 @@ const DesktopSidebar = () => {
     return (
         <Sidebar collapsible="icon" className="hidden md:flex no-print">
             <SidebarContent>
-                <SidebarHeader className="h-14 lg:h-[60px] border-b justify-center p-2">
-                    <div className="flex items-center justify-center gap-2">
+                <SidebarHeader className="h-14 lg:h-[60px] border-b justify-center">
+                    <div className="flex items-center group-data-[collapsible=icon]:justify-center">
                         <Logo className="group-data-[collapsible=icon]:hidden" />
                         <Logo className="hidden group-data-[collapsible=icon]:flex" hideText />
                     </div>
                 </SidebarHeader>
-                <SidebarMenu className="flex-1">
+                <SidebarMenu className="flex-1 px-2">
                     {navItems.map(item => (
                         <SidebarMenuItem key={item.label}>
                             <Link href={item.href}>
