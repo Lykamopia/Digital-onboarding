61048422a913d4cd6982b697355cc08e853a5b1a
Alex Yaj <www.alex94lykam@gmail.com>
2025-12-22 10:02:01 +0000

    reduce the width of the sidebar to be minimal and professional

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -22,9 +22,9 @@ import {
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH_EXPANDED = "280px"
-const SIDEBAR_WIDTH_COLLAPSED = "68px"
-const SIDEBAR_WIDTH_MOBILE = "18rem"
+const SIDEBAR_WIDTH_EXPANDED = "240px"
+const SIDEBAR_WIDTH_COLLAPSED = "60px"
+const SIDEBAR_WIDTH_MOBILE = "16rem"
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 type SidebarContext = {
