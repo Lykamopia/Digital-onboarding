de3f205b6c79c1e1e698e13c64237fd1fe144bf3
Alex Yaj <www.alex94lykam@gmail.com>
2025-12-22 10:57:58 +0000

    Ensure the sidebar icons and the logo have consistent padding and margin

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -91,7 +91,7 @@ const DesktopSidebar = () => {
     return (
         <Sidebar collapsible="icon" className="hidden md:flex no-print">
             <SidebarContent>
-                <SidebarHeader className="h-14 lg:h-[60px] border-b justify-center">
+                <SidebarHeader className="h-14 lg:h-[60px] border-b justify-center p-2">
                     <div className="flex items-center gap-2 group-data-[collapsible=icon]:justify-center">
                         <Logo className="group-data-[collapsible=icon]:hidden" />
                         <Logo className="hidden group-data-[collapsible=icon]:flex" hideText />
