e4b095d95a0d15d6a6486749aaf8f461f9092d59
Alex Yaj <www.alex94lykam@gmail.com>
2025-12-23 08:34:24 +0000

    make the loading animation more compact and meaningfulll and also beautf

--- src/components/honeycomb-loader.tsx
+++ src/components/honeycomb-loader.tsx
@@ -4,13 +4,12 @@
 export function HoneycombLoader() {
   return (
     <div className="honeycomb-loader">
-      <div className="honeycomb-cell"></div>
-      <div className="honeycomb-cell"></div>
-      <div className="honeycomb-cell"></div>
-      <div className="honeycomb-cell"><div className="drip"></div></div>
-      <div className="honeycomb-cell"></div>
-      <div className="honeycomb-cell"></div>
-      <div className="honeycomb-cell"></div>
+        <div className="honeycomb-cell">
+            <div className="honeycomb-cell__inner">
+                <div className="honeycomb-cell__honey"></div>
+            </div>
+        </div>
+        <div className="text-sm mt-4 font-semibold text-primary/80 tracking-widest">LOADING...</div>
     </div>
   );
 }
