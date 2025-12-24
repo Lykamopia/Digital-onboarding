e3d7859f143d534403252118062b6cbbb769b047
Lykamopia <www.alex94lykam@gmail.com>
2025-12-24 12:14:11 +0000

    Redesign the loader by replacing the current honeycomb style with a Tele

--- src/components/honeycomb-loader.tsx
+++ src/components/honeycomb-loader.tsx
@@ -3,13 +3,18 @@
 
 export function HoneycombLoader() {
   return (
-    <div className="honeycomb-loader">
-        <div className="honeycomb-cell">
-            <div className="honeycomb-cell__inner">
-                <div className="honeycomb-cell__honey"></div>
-            </div>
-        </div>
-        <div className="text-sm mt-4 font-semibold text-primary/80 tracking-widest">LOADING...</div>
+    <div className="flex flex-col items-center justify-center gap-4">
+      <div className="telegram-loader">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          className="telegram-loader__plane"
+        >
+          <path d="M22 2L11 13" />
+          <path d="m22 2-7 20-4-9-9-4Z" />
+        </svg>
+      </div>
+      <div className="text-sm font-semibold text-primary/80 tracking-widest">LOADING...</div>
     </div>
   );
 }
