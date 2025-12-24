5df0f9c60b6779db11701afd3c888ec3572a1018
Lykamopia <www.alex94lykam@gmail.com>
2025-12-24 12:48:07 +0000

    in the login page only the fallback ''Nib Memo Logo' is displayed not th

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,11 +5,13 @@ export interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {
   hideText?: boolean;
 }
 
-export default function Logo({ hideText = false, className }: LogoProps) {
+export default function Logo({ className, hideText = false }: LogoProps) {
   return (
-    <div className={cn("flex items-center gap-2 text-lg font-semibold text-primary", className)}>
+    <div className={cn('flex items-center gap-2', className)}>
       <Image src="/Logo.png" alt="Nib Memo Logo" width={32} height={32} />
-      {!hideText && <span className="font-headline">Nib Memo</span>}
+      {!hideText && (
+        <span className="text-xl font-bold text-primary">Nib Memo</span>
+      )}
     </div>
   );
 }
