1072614f06d62d1c0512cc802ea56e7695ac576e
Lykamopia <www.alex94lykam@gmail.com>
2025-12-26 07:44:08 +0000

    the email is not being sent fix this issue my email configuration in .en

--- src/lib/email.ts
+++ src/lib/email.ts
@@ -5,7 +5,7 @@ import type { Memo, User } from './types';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: Number(process.env.EMAIL_PORT),
-  secure: Number(process.env.EMAIL_PORT) === 465, // true for 465, false for other ports
+  secure: false, // true for 465, false for other ports like 587
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
