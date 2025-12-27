87257298d58b5fd25c2a5a2be52a0233552e1f48
Lykamopia <www.alex94lykam@gmail.com>
2025-12-27 07:33:54 +0000

    for light theme use light gray bg for the sidebar and the same dark vers

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,7 +57,7 @@ export default {
           '5': 'hsl(var(--chart-5))',
         },
         sidebar: {
-          DEFAULT: 'hsl(var(--background))',
+          DEFAULT: 'hsl(var(--sidebar-background))',
           foreground: 'hsl(var(--foreground))',
           primary: 'hsl(var(--primary))',
           'primary-foreground': 'hsl(var(--primary-foreground))',
