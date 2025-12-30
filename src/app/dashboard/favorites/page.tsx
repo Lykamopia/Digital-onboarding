297656280874bf9a068edd33b3815800680f9fc2
Lykamopia <www.alex94lykam@gmail.com>
2025-12-30 12:44:30 +0000

    i can't see the favourite page in the sidebar and also when manually nav

--- src/app/dashboard/favorites/page.tsx
+++ src/app/dashboard/favorites/page.tsx
@@ -2,9 +2,14 @@
 import { getDashboardData, getLoggedInUser } from '@/app/actions/memo';
 import MainDashboard from '../main-dashboard';
 import type { MemoWithActivity, User } from '@/lib/types';
+import { redirect } from 'next/navigation';
 
 export default async function FavoritesPage({ searchParams }: { searchParams: { [key: string]: string | string[] | undefined }}) {
     const user = await getLoggedInUser();
+    if (!user || !user.role?.permissions.includes('view_dashboard')) {
+        redirect('/dashboard/access-denied');
+    }
+
     const show = typeof searchParams.show === 'string' ? searchParams.show : '';
     const initialMemos = await getDashboardData('favorites', '', '', { from: undefined, to: undefined }, [], show);
 
