693134cd013a85f0d427d2e550bcdfcfafcc49bf
Lykamopia <www.alex94lykam@gmail.com>
2026-01-02 13:10:04 +0000

    Implement strong HTTP security headers at the server level to harden the

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,30 @@
 
 import type {NextConfig} from 'next';
 
+const securityHeaders = [
+    {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=63072000; includeSubDomains; preload',
+    },
+    {
+        key: 'X-Permitted-Cross-Domain-Policies',
+        value: 'none',
+    },
+    {
+        key: 'Cross-Origin-Embedder-Policy',
+        value: 'require-corp',
+    },
+    {
+        key: 'Cross-Origin-Opener-Policy',
+        value: 'same-origin',
+    },
+    {
+        key: 'Cross-Origin-Resource-Policy',
+        value: 'same-origin',
+    }
+];
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -30,6 +54,15 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
   },
 };
 
