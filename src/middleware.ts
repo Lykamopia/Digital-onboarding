34f9042dfac40565ca5c44e710d02608cc07c42c
Lykamopia <www.alex94lykam@gmail.com>
2026-02-07 08:16:45 +0000

    it is still causing the '9261-fe8359337c2bd47f.js:1 Applying inline styl

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ function generateCsp(nonce: string) {
     const policies = {
         'default-src': ["'self'"],
         'script-src': ["'self'", `'nonce-${nonce}'`, "'strict-dynamic'", "'sha256-n46vPwSWuMC0W703pBofImv82Z26xo4LXymv0E9caPk='"],
-        'style-src': ["'self'", "https://fonts.googleapis.com", `'nonce-${nonce}'`],
+        'style-src': ["'self'", "https://fonts.googleapis.com", "'unsafe-inline'"],
         'img-src': ["'self'", "data:", "https://images.unsplash.com", "https://picsum.photos", "https://cdn.brandfetch.io"],
         'connect-src': ["'self'", "ws://localhost:3011", "http://localhost:3011"],
         'font-src': ["'self'", "https://fonts.gstatic.com"],
